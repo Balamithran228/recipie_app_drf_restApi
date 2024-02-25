926f93d30194052177c4022004cb36169050adf5
Balamithran228 <ssbalabhavani228@gmail.com>
2024-02-25 21:49:29 +0530

    admin part modifcation

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,11 +5,8 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
-
-
 class AdminSiteTests(TestCase):
     """Tests for Django admin."""
-
     def setUp(self):
         """Create user and client."""
         self.client = Client()
@@ -23,7 +20,6 @@ class AdminSiteTests(TestCase):
             password='testpass123',
             name='Test User'
         )
-
     def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
@@ -31,3 +27,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
